a39549a4c4bc7f944d518fdc03d41c747ebd73bd
André Fragoso <andrefsantosprog@gmail.com>
2024-06-07 15:35:14 -0300

    User entity and resource

---
+++ src/main/java/com/myproject/project/resources/UserResources.java
@@ -0,0 +1,24 @@
+package com.myproject.project.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myproject.project.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		
+		User u = new User(1L, "André Fragoso", "andre@andre.gmail.com", "99999999", "12345");
+		return ResponseEntity.ok().body(u);
+		
+			
+		
+	}
+
+}
