9aaad30d4329bd1926c6aa3743a7acb83bdedce1
André Fragoso <andrefsantosprog@gmail.com>
2024-06-13 11:39:21 -0300

    [200~Category

---
+++ src/main/java/com/myproject/project/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.myproject.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.myproject.project.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ src/main/java/com/myproject/project/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.myproject.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.myproject.project.entities.Category;
+import com.myproject.project.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository; 
+    
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
